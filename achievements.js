7baa262bdb5922fb0651734723cc1451357b9188
Varinder-Dhillon0 <98728916+Varinder-Dhillon0@users.noreply.github.com>
2024-09-03 10:45:35 +0530

    all errors resolved

--- achievements.js
+++ achievements.js
@@ -1,10 +1,12 @@
 const db = require("./database");
+const { ObjectId } = require("mongodb");
 
 // Get all achievements
 const getAchievements = async (req, res) => {
     try {
-        const achievements = await db.collection("achievements").find({}).toArray();
-        res.render("achievements", { achievements });
+        const achievements = await db.collection("achievements").find().toArray();
+        console.log(achievements);
+        res.render("achievements_view", { achievements : achievements });
     } catch (err) {
         console.error(err);
         res.status(500).send("Error retrieving achievements");
@@ -13,21 +15,17 @@ const getAchievements = async (req, res) => {
 
 // Add a new achievement
 const addAchievement = (req, res) => {
-    res.render("achievement_add");
+    res.render("achievements_add_view");
 };
 
 const addAchievementPOST = async (req, res) => {
     try {
-        const { first_name, last_name, email, password, registered_at, quizzes_done_id, quizzes_created_id, roles } = req.body;
+        const { user_id, achievement_name, description, earned_at } = req.body;
         await db.collection("achievements").insertOne({
-            first_name,
-            last_name,
-            email,
-            password,
-            registered_at: new Date(registered_at),
-            quizzes_done_id: quizzes_done_id.split(",").map(id => new db.ObjectId(id)),
-            quizzes_created_id: quizzes_created_id.split(",").map(id => new db.ObjectId(id)),
-            roles: roles.split(","),
+            user_id : user_id,
+            achievement_name : achievement_name,
+            description : description,
+            earned_at : earned_at
         });
         req.session.msg = "Achievement added successfully!";
         res.redirect("/achievement");
@@ -40,9 +38,9 @@ const addAchievementPOST = async (req, res) => {
 // Edit an achievement
 const editAchievement = async (req, res) => {
     try {
-        const id = new db.ObjectId(req.params.id);
-        const achievement = await db.collection("achievements").findOne({ _id: id });
-        res.render("achievement_edit", { achievement });
+        const {id} = req.params;
+        const achievement = await db.collection("achievements").findOne({ _id: new ObjectId(id) });
+        res.render("achievement_edit_view", { achievement });
     } catch (err) {
         console.error(err);
         res.status(500).send("Error retrieving achievement");
@@ -51,20 +49,16 @@ const editAchievement = async (req, res) => {
 
 const editAchievementPOST = async (req, res) => {
     try {
-        const id = new db.ObjectId(req.body._id);
-        const { first_name, last_name, email, password, registered_at, quizzes_done_id, quizzes_created_id, roles } = req.body;
+        const {id,user_id, achievement_name, description, earned_at} = req.body;
+
         await db.collection("achievements").updateOne(
-            { _id: id },
+            { _id: new ObjectId(id) },
             {
                 $set: {
-                    first_name,
-                    last_name,
-                    email,
-                    password,
-                    registered_at: new Date(registered_at),
-                    quizzes_done_id: quizzes_done_id.split(",").map(id => new db.ObjectId(id)),
-                    quizzes_created_id: quizzes_created_id.split(",").map(id => new db.ObjectId(id)),
-                    roles: roles.split(","),
+                    user_id : user_id,
+                    achievement_name : achievement_name,
+                    description : description,
+                    earned_at : earned_at
                 },
             }
         );
@@ -79,8 +73,8 @@ const editAchievementPOST = async (req, res) => {
 // Delete an achievement
 const deleteAchievement = async (req, res) => {
     try {
-        const id = new db.ObjectId(req.params.id);
-        await db.collection("achievements").deleteOne({ _id: id });
+        const id = req.params.id;
+        await db.collection("achievements").deleteOne({ _id: new ObjectId(id) });
         req.session.msg = "Achievement deleted successfully!";
         res.redirect("/achievement");
     } catch (err) {
